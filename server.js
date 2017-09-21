fd5233e1ea4998452861df95b0748badc189e1a0
scott <scott.scialabba@gmail.com>
2017-09-20 23:36:28 -0700

    select port dynamically, or default to 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var cors = require('cors');
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 var routes = require('./routes/codeChallengeRoutes'); //importing route
 var corsOptions;
 

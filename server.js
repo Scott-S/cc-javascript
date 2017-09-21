85bbafe345ae49388a4e025fd615a61eb27142bb
scott <scott.scialabba@gmail.com>
2017-09-20 23:11:16 -0700

    remove bodyParser package, not necessary

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 var cors = require('cors');
-var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var port = 3000;
 var routes = require('./routes/codeChallengeRoutes'); //importing route
 var corsOptions;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 //uncomment this option to allow CORS from zesty.engineering exclusively
 //corsOptions = {
